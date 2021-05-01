10bd4896300faadcb66fe9a7a199da7c4792f07f
Hazem Karoui <46596292+HazemKaroui@users.noreply.github.com>
2021-05-01 23:40:20 +0100

    latest spec is v0.4

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import json
 from datetime import datetime, timedelta, timezone
 import time
 
-__iid__ = "PythonInterface/v0.2"
+__iid__ = "PythonInterface/v0.4"
 __title__ = "Forecast"
 __prettyname__ = "Forecast"
 __version__ = "0.0.2"
